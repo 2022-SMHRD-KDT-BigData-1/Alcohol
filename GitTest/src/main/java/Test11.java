aaf2329b8936852d85931df6fa7ffcf355ead9cf
smhrd <smhrd@211.63.240.114>
2022-01-20 16:34:27 +0900

    수정했습니다.

---
+++ GitTest/src/main/java/Test11.java
@@ -0,0 +1,13 @@
+
+public class Test11 {
+
+	public static void main(String[] args) {
+		String name ="박건하";
+		String number = "010-1234-5678";
+		String address="광주 동구";
+		
+		System.out.println("안녕하세여 저는 "+name+"입니다.  저의 폰 넘버는 "+number+"이고 주소는 "+address+" 입니다." );
+		
+	}
+
+}
