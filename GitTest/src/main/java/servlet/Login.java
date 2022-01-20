119bd3774c200127e0b393658ee7bb4b0e10c631
smhrd <smhrd@59.0.124.155>
2022-01-20 16:56:00 +0900

    로그인 서블릿, SQL

--- GitTest/src/main/java/servlet/Login.java
+++ GitTest/src/main/java/servlet/Login.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class memberDAO
  */
 @WebServlet("/memberDAO")
-public class memberDAO extends HttpServlet {
+public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	/**
